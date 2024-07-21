68dc9a55dca2f13c10f4a7641678ba9a44c34c28
Hansana Sandakelum <95872422+HansanaSandakelum@users.noreply.github.com>
2024-07-21 22:14:44 +0530

    create upsateuser component

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -12,7 +12,10 @@ const Users = () => {
   return (
     <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
       <div className="w-50 bg-white rounded p-3">
-        <Link to='/create' className='btn btn-success' > Add +</Link>
+        <Link to="/create" className="btn btn-success">
+          {" "}
+          Add +
+        </Link>
         <table className="table">
           <thead>
             <tr>
@@ -29,7 +32,9 @@ const Users = () => {
                 <td>{user.Email}</td>
                 <td>{user.Age}</td>
                 <td>
-                  <button>Edit</button>
+                  <Link to="/create" className="btn btn-success">
+                    Update
+                  </Link>
                   <button>Delete</button>
                 </td>
               </tr>
